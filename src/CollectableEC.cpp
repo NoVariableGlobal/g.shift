03d0be61011259df1400c928baa45acfb0876a60
Miriam <mirileis@ucm.es>
2020-04-01 12:20:45 +0200

    creado collectableEC

---
+++ src/CollectableEC.cpp
@@ -0,0 +1,47 @@
+#include "CollectableEC.h"
+#include "ComponentsManager.h"
+#include "Entity.h"
+#include "FactoriesFactory.h"
+#include "RigidbodyPC.h"
+#include "Scene.h"
+
+#include <json.h>
+
+void CollectableEC::checkEvent() {
+    if (reinterpret_cast<RigidbodyPC*>(father->getComponent("RigidbodyPC"))
+            ->collidesWith("Player")) {
+        scene->deleteEntity(father);
+    }
+}
+
+int CollectableEC::getValue()
+{
+    return value_;
+}
+
+void CollectableEC::setValue(int value)
+{
+    value_ = value;
+}
+
+// FACTORY INFRASTRUCTURE
+CollectableECFactory::CollectableECFactory() = default;
+
+Component* CollectableECFactory::create(Entity* _father, Json::Value& _data,
+                                  Scene* _scene) {
+    CollectableEC* collectable = new CollectableEC();
+    _scene->getComponentsManager()->addEC(collectable);
+
+    collectable->setFather(_father);
+    collectable->setScene(_scene);
+
+    if (!_data["value"].asInt())
+        throw std::exception("CollectableEC: value is not an int");
+    collectable->setValue(_data["value"].asInt());
+
+    collectable->setActive(true);
+
+    return collectable;
+};
+
+DEFINE_FACTORY(CollectableEC);
---
+++ src/CollectableEC.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "EventComponent.h"
+#include "Factory.h"
+
+DECLARE_FACTORY(CollectableEC);
+
+class CollectableEC : public EventComponent {
+  private:
+      // value of the collectable
+      int value_;
+
+  public:
+      CollectableEC() = default;
+    ~CollectableEC() = default;
+
+    virtual void checkEvent();
+
+    // get the value of the collectable
+    int getValue();
+    // set the value of the collectable
+    void setValue(int value);
+};
