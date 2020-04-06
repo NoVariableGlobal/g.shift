12859d63d9f4df57af2c3ed73fda725539b326a9
Alejandro Placer <aplazer@ucm.es>
2020-04-06 13:05:57 +0200

    fixed errors

--- src/ChangeGravityIC.cpp
+++ src/ChangeGravityIC.cpp
@@ -19,70 +19,23 @@ ChangeGravityIC::ChangeGravityIC() {}
 ChangeGravityIC::~ChangeGravityIC() {}
 
 void ChangeGravityIC::handleInput(const SDL_Event& _event) {
-<<<<<<< HEAD
-<<<<<<< master
     if (_event.type == SDL_KEYDOWN && _event.key.keysym.sym == SDLK_SPACE) {
         movingIzq = !movingIzq;
-=======
-	if (_event.type == SDL_KEYDOWN && _event.key.keysym.sym == SDLK_SPACE) {
-		movingIzq = !movingIzq;
->>>>>>> map-design
 
-		RigidbodyPC* body =
-			dynamic_cast<RigidbodyPC*>(father->getComponent("RigidbodyPC"));
+        RigidbodyPC* body =
+            dynamic_cast<RigidbodyPC*>(father->getComponent("RigidbodyPC"));
 
-<<<<<<< HEAD
         body->setGravity(!movingIzq ? Ogre::Vector3(speed, 0.0f, 0.0f)
                                     : Ogre::Vector3(-speed, 0.0f, 0.0f));
-=======
-		body->setGravity(!movingIzq ? Ogre::Vector3(speed, 0.0f, 0.0f)
-			: Ogre::Vector3(-speed, 0.0f, 0.0f));
->>>>>>> map-design
 
-		Ogre::Vector3 velocity = body->getLinearVelocity();
-		body->setLinearVelocity(Ogre::Vector3(0.0f, velocity.y, 0.0f));
+        Ogre::Vector3 velocity = body->getLinearVelocity();
 
-		dynamic_cast<TridimensionalObjectRC*>(
-			father->getComponent("TridimensionalObjectRC"))
-			->setMaterial(!movingIzq ? mRight : mLeft);
-	}
-	if (_event.type == SDL_KEYDOWN && _event.key.keysym.sym == SDLK_a) {
-		RigidbodyPC* body =
-			dynamic_cast<RigidbodyPC*>(father->getComponent("RigidbodyPC"));
+        body->setLinearVelocity(Ogre::Vector3(0.0f, velocity.y, 0.0f));
 
-<<<<<<< HEAD
         dynamic_cast<TridimensionalObjectRC*>(
             father->getComponent("TridimensionalObjectRC"))
             ->setMaterial(!movingIzq ? mRight : mLeft);
     }
-=======
-	if (_event.type == SDL_KEYDOWN && _event.key.keysym.sym == SDLK_SPACE) {
-		movingIzq = !movingIzq;
-
-		RigidbodyPC* body =
-			dynamic_cast<RigidbodyPC*>(father->getComponent("RigidbodyPC"));
-
-		body->setGravity(!movingIzq ? Ogre::Vector3(speed, 0.0f, 0.0f)
-			: Ogre::Vector3(-speed, 0.0f, 0.0f));
-
-		Ogre::Vector3 velocity = body->getLinearVelocity();
-		body->setLinearVelocity(Ogre::Vector3(0.0f, velocity.y, 0.0f));
-
-		dynamic_cast<TridimensionalObjectRC*>(
-			father->getComponent("TridimensionalObjectRC"))
-			->setMaterial(!movingIzq ? mRight : mLeft);
-	}
-	if (_event.type == SDL_KEYDOWN && _event.key.keysym.sym == SDLK_a) {
-		RigidbodyPC* body =
-			dynamic_cast<RigidbodyPC*>(father->getComponent("RigidbodyPC"));
-
-		body->setLinearVelocity(Ogre::Vector3(0.0f, 0.0f, 0.0f));
-	}
->>>>>>> map reworked
-=======
-		body->setLinearVelocity(Ogre::Vector3(0.0f, 0.0f, 0.0f));
-	}
->>>>>>> map-design
 }
 
 void ChangeGravityIC::setSpeed(float _speed) { speed = _speed; }
@@ -90,7 +43,7 @@ void ChangeGravityIC::setSpeed(float _speed) { speed = _speed; }
 void ChangeGravityIC::setMaterialLeft(std::string _mLeft) { mLeft = _mLeft; }
 
 void ChangeGravityIC::setMaterialRight(std::string _mRight) {
-	mRight = _mRight;
+    mRight = _mRight;
 }
 
 // FACTORY INFRASTRUCTURE
@@ -98,29 +51,29 @@ void ChangeGravityIC::setMaterialRight(std::string _mRight) {
 ChangeGravityICFactory::ChangeGravityICFactory() = default;
 
 Component* ChangeGravityICFactory::create(Entity* _father, Json::Value& _data,
-	Scene* scene) {
-	ChangeGravityIC* changeGravity = new ChangeGravityIC();
-	scene->getComponentsManager()->addIC(changeGravity);
+                                          Scene* scene) {
+    ChangeGravityIC* changeGravity = new ChangeGravityIC();
+    scene->getComponentsManager()->addIC(changeGravity);
 
-	changeGravity->setFather(_father);
-	changeGravity->setScene(scene);
+    changeGravity->setFather(_father);
+    changeGravity->setScene(scene);
 
-	if (!_data["speed"].isDouble())
-		throw std::exception("ChangeGravityIC: speed is not a double");
+    if (!_data["speed"].isDouble())
+        throw std::exception("ChangeGravityIC: speed is not a double");
 
-	changeGravity->setSpeed(_data["speed"].asDouble());
+    changeGravity->setSpeed(_data["speed"].asDouble());
 
-	if (!_data["materialL"].isString())
-		throw std::exception("ChangeGravityIC: materialL is not a string");
+    if (!_data["materialL"].isString())
+        throw std::exception("ChangeGravityIC: materialL is not a string");
 
-	changeGravity->setMaterialLeft(_data["materialL"].asString());
+    changeGravity->setMaterialLeft(_data["materialL"].asString());
 
-	if (!_data["materialR"].isString())
-		throw std::exception("ChangeGravityIC: materialR is not a string");
+    if (!_data["materialR"].isString())
+        throw std::exception("ChangeGravityIC: materialR is not a string");
 
-	changeGravity->setMaterialRight(_data["materialR"].asString());
+    changeGravity->setMaterialRight(_data["materialR"].asString());
 
-	return changeGravity;
+    return changeGravity;
 };
 
 DEFINE_FACTORY(ChangeGravityIC);
